47635fc049007890361d2e6ff4350fc4cf8615cd
FAVOU-R <53179330+FAVOU-R@users.noreply.github.com>
2026-08-07 00:23:21 +0000

    Delete main.py

--- main.py
+++
@@ -1,7 +0,0 @@
-import os
-import uvicorn
-from app.main import app
-
-if __name__ == "__main__":
-    port = int(os.getenv("PORT", 10000))
-    uvicorn.run("app.main:app", host="0.0.0.0", port=port)
